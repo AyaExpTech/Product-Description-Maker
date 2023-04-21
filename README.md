6f2f917e113927ce20481fb9cd5f302b44e56d3a
AyaExpTech <ayasakaexpresstech@gmail.com>
2023-04-21 15:52:56 +0900

    2023-04-21 `v0.1.0` - 非公開リポジトリから移転 - 入力クリアボタンを追加(`index.html`) - 再描画ボタンのアイコンを🔃系から✎系に変更(`index.html`) - 表に交互の背景色を適用(`style.css`) - テーブル欄に無入力の場合は表を表示しない仕様に変更(`script.js`, `style.css`)

---
+++ README.md
@@ -0,0 +1,33 @@
+# AXT_Product-Description-Maker
+
+製品などの簡易的な説明画像を作成するツールです。  
+製品に限らず何でも使えると思います。
+
+## 利用規約
+
+Copyright (c) 2023-  
+AyaExpTech, Ayasaka-Koto
+
+以下の規約のいずれかに従ってください。  
+Follow one of the following licenses.
+
+- "AeTOS for Seller" …… https://axtech.dev/license/#aetos-for-seller
+
+※このツールを用いて生成された画像の著作権はユーザーに帰属します。
+
+## 更新履歴
+
+既知のバグ
+- 特定環境でダウンロード画像が正常に描画されない不具合あり？
+    - html2canvasの安定感……
+
+2023-04-21 `v0.1.0`
+- 非公開リポジトリから移転
+- 入力クリアボタンを追加(`index.html`)
+- 再描画ボタンのアイコンを🔃系から✎系に変更(`index.html`)
+- 表に交互の背景色を適用(`style.css`)
+- テーブル欄に無入力の場合は表を表示しない仕様に変更(`script.js`, `style.css`)
+
+## 使い方
+
+察してください
---
+++ script.js
@@ -0,0 +1,62 @@
+const $ = (s) => document.querySelectorAll(s); // 少しだけjQueryライクに書く
+
+/**
+ * プレビュー画面を再描画します
+ */
+const redrawPreview = () => {
+    return new Promise((resolve) => {
+        /* == 商品名 == */
+        $(`.pre>h2`)[0].innerText = $(`.in>.name>input`)[0].value;
+        /* == 商品画像 == */
+        const reader = new FileReader();
+        if (Boolean($(`.in>.image>input`)[0].files[0])) {
+            reader.readAsDataURL($(`.in>.image>input`)[0].files[0]);
+            reader.onload = function () {
+                $(`.pre>div>img`)[0].src = reader.result;
+            };
+        }
+        /* == 商品説明 == */
+        $(`.pre>div>ul`)[0].innerHTML = "";
+        $(`.in>.desc>input`).forEach(e => {
+            let add = document.createElement('li');
+            add.innerHTML = e.value;
+            $(`.pre>div>ul`)[0].appendChild(add);
+        });
+        /* == 商品リスト == */
+        const listText = $(`.in>.list>textarea`)[0].value;
+        const listArray = _AXT.conv.CSV_to_2dArray(listText);
+        let addInTable = "";
+        listArray.forEach(e => {
+            let addInTr = "";
+            e.forEach(f => {
+                addInTr += `<td>${f}</td>`;
+            });
+            addInTable += `<tr>${addInTr}</tr>`;
+        });
+        $(`.pre>table`)[0].innerHTML = addInTable == "<tr><td></td></tr>" ? "" : addInTable;
+        resolve(); // すべて終わったら解決する
+    })
+};
+
+/**
+ * 説明記入欄を増やします
+ */
+const addDescInput = () => {
+    let add = document.createElement('input');
+    add.type = `text`;
+    $(`.in>.desc>button`)[0].before(add);
+}
+
+/**
+ * 画像をダウンロードします
+ */
+const download = () => {
+    redrawPreview().then(() => {
+        html2canvas($(`.pre`)[0]).then(c => {
+            let a = document.createElement("a");
+            a.href = c.toDataURL("image/jpg", 1);
+            a.download = "image.jpg";
+            a.click();
+        });
+    });
+}
